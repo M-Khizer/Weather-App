38ad07a03f45c29d48f7862c4db4c026357a2a06
Muhammad Khizer <89213198+M-Khizer@users.noreply.github.com>
2022-02-15 17:34:29 +0500

    Update App.js

--- App.js
+++ App.js
@@ -8,31 +8,41 @@ function App() {
   const key='gMPX1smQKRS18O60GFjexssclAiaopPT'
   const [cityname,setCity]=useState('karachi');
   const [cityData,setCityData]=useState();
-  
+  const [id,setId]=useState('');
   const [search,setSearch]=useState('karachi')
-
-  // console.log(cityData)
-  // useEffect(() => {
-    
-  //   const baseURL="http://dataservice.accuweather.com/currentconditions/v1/"
-  //   const query=`${id}?apikey=${key}`
-
-  //   const res= await fetch(baseURL + query)
-  //   const data = await res.json()
-
-  //   return data[0]
-    
-  // });
-
+  const [weatherData,setWeatherData]=useState();
+  // setId(cityData.Key)
+  // console.log(weatherData)
   useEffect(() => {
     
+    const baseURL="http://dataservice.accuweather.com/currentconditions/v1/"
+    const query=`261158?apikey=${key}`
+
+    const weatherDetails=async()=>{
+      
+      const res= await fetch(baseURL + query).catch(e=>console.log(e))
+      const data = await res.json()
+
+      setWeatherData(data[0])
+    
+    }
+    weatherDetails()
+  },[]);
+
+  useEffect(() => {
+    // navigator.geolocation.getCurrentPosition(position=>{
+    //   console.log(position.coords.latitude)
+    //   console.log(position.coords.longitude)
+
+    // })
     const baseURL="http://dataservice.accuweather.com/locations/v1/cities/search"
 
     const query=`?apikey=${key}&q=${search}`
 
-    
+
     const CityDetails= async ()=>{
-      const fetchData=await fetch(baseURL+query);
+    
+      const fetchData=await fetch(baseURL+query).catch(err=>console.log(err));
       const result= await fetchData.json()
       // setCityData([ ])
       setCityData(result[0])
@@ -41,14 +51,15 @@ function App() {
 
       // console.log(result[0])
     }
+
     CityDetails()
     
     // console.log(cityData)
-
+    // setKey(cityData.Key)
+    // console.log(keys)  
+    
 
   },[search]);
-  
-  
   // const getWeatherDetails= async (id)=>{
     
   // }
@@ -57,13 +68,21 @@ function App() {
 
    
   //   }
+  // console.log(cityData.Key)
 
     return(
       <>
         <Searchform cityname={cityname} setCity={setCity} setSearch={setSearch}/>
-        <DisplayWeather cityData={cityData}/>
+
+      {
+        (typeof(cityData) !== 'undefined') ?
+          <DisplayWeather cityData={cityData} weatherData={weatherData} setId={setId}/>
+        : console.log('Undefined')
+      }
+        {/* {cityData.map(data=>)} */}
       </>
     )
+
 
   }
 
