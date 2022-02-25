e4e8665d6868183bf32fe93bad113ba6baa733c3
Muhammad Khizer <89213198+M-Khizer@users.noreply.github.com>
2022-02-25 05:13:14 -0800

    Update DisplayWeather.js

--- DisplayWeather.js
+++ DisplayWeather.js
@@ -1,22 +1,18 @@
-import React, { useState } from 'react';
+ import React, { useState } from 'react';
 
 function DisplayWeather({cityData,weatherData,setId}) {
-  // const [key,setKey]=useState()
-  // const [loading,setLoading]=useState(true)
-  // setKey(cityData)
-  console.log(cityData.Key)
-  // setId(cityData.Key)
+ 
   console.log(weatherData)
-  // setKey(cityData)
-  // console.log(key)
-//  console.log(cityData.Key)
-//  cityData.map(data=>{
-//    console.log(data.Key)
-//  })
+
+  console.log(cityData)
+  // setId(cityData.Key)
   return(
   <div>
-    <p>{cityData.EnglishName}</p>
-   <p>{weatherData.WeatherText}</p>
+    <p>{cityData?.Country.EnglishName}</p>
+    <p>{cityData?.Key}</p>
+
+   <p>{weatherData?.Temperature.Metric.Value} &deg;c</p>
+
   </div>
   )
 }
