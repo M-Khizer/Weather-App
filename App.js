c1e3c8e6af0e9be719382244747299a41f19d365
Muhammad Khizer <89213198+M-Khizer@users.noreply.github.com>
2022-03-01 07:37:02 -0800

    Update App.js

--- App.js
+++ App.js
@@ -2,55 +2,21 @@ import { useState,useEffect } from 'react';
 import './App.css';
 import DisplayWeather from './displayweather';
 import Searchform from './searchform';
-// import searchform from './searchform';
 
 function App() {
-  const key='BDlk68JVkVkyp0j9GxAQyG8sviskZ2VZ'
+  
+  const [keys,setKeys]=useState('ATTQbEtKYLFkdQJ1PBBSxzJFqHmWKhaH')
   const [cityname,setCity]=useState('karachi');
   const [cityData,setCityData]=useState();
-  const [id,setId]=useState('');
   const [search,setSearch]=useState('karachi')
-  const [weatherData,setWeatherData]=useState();
-  
+  const [id,setId]=useState('');
+
   // setId(cityData?.Key)
-  // console.log(id)
-  // 261158
-
-  useEffect(() => {
-    
-    const baseURL="http://dataservice.accuweather.com/currentconditions/v1/"
-    const query=`261158?apikey=${key}`
-    const controller= new AbortController()
-    const signal=controller.signal
-
-    const weatherDetails= async ()=>{
-      
-      const res= await fetch(baseURL+query,{
-        signal:signal
-      }).catch(e=>{
-        if(e.name === 'AbortError'){
-          console.log('Abort success')
-        }
-        else{
-          console.log(e)
-        }
-      })
-      const data = await res.json()
-      setWeatherData(data[0])
-      // console.log(weatherData)
-
-      return ()=>{
-        controller.abort()
-      }
-    }
-    weatherDetails()
-  },[]);
-
   useEffect(() => {
   
     const baseURL="http://dataservice.accuweather.com/locations/v1/cities/search"
 
-    const query=`?apikey=${key}&q=${search}`
+    const query=`?apikey=${keys}&q=${search}`
 
     const controller= new AbortController()
     const signal=controller.signal
@@ -69,11 +35,9 @@ function App() {
         }
       });
       const result= await fetchData.json()
-      console.log(fetchData)
-      console.log(result)
 
       setCityData(result[0])
-
+     
       return ()=>{
         controller.abort()
       }
@@ -89,9 +53,11 @@ function App() {
 
       {
         typeof(cityData) !== "undefined" ? (
-        <DisplayWeather cityData={cityData} weatherData={weatherData} setId={setId}/>)
-        : <p>Loading...</p>
+        <DisplayWeather cityData={cityData} keys={keys} setId={setId} id={id}/>)
+        : <p>City not found</p>
       }
+     
+
       </>
     )
 
